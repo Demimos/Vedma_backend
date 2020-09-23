d8d6a5174c805fcd66dc0a9f5e15cadd58fdcdf4
Demimos <43846185+Demimos@users.noreply.github.com>
2020-09-23 21:20:03 +0300

    kestrel configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -16,11 +18,16 @@ namespace Vedma_backend
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .UseKestrel(options =>
+            {
+                options.Listen(IPAddress.Loopback, 5000);
+                options.Listen(IPAddress.Loopback, 5001, listenOptions =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    listenOptions.UseHttps();
                 });
+            });
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -52,6 +52,7 @@ namespace Vedma_backend
             app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseAuthentication();
 
             app.UseForwardedHeaders(new ForwardedHeadersOptions
             {

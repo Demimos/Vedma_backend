41629cbc8d4c97ff2604c526ffed8731d10ff1bf
Demimos <43846185+Demimos@users.noreply.github.com>
2020-09-27 23:48:03 +0300

    додбавил вложения в метод получяения детальной информации о персонаже. Из списка - убрал

--- Controllers/CharSheetController.cs
+++ Controllers/CharSheetController.cs
@@ -26,14 +26,14 @@ namespace Vedma_backend.Controllers
         [HttpGet]
         public async Task<IEnumerable<CharSheet>> Get()
         {
-            return await Db.CharSheets.Include(c => c.Properties).AsNoTracking().ToListAsync();
+            return await Db.CharSheets.AsNoTracking().ToListAsync();
         }
 
         // GET api/<CharSheetController>/5
         [HttpGet("{id}")]
         public async Task<ActionResult<CharSheet>> Get(int id)
         {
-            var sheet = await Db.CharSheets.AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
+            var sheet = await Db.CharSheets.Include(c => c.Properties).AsNoTracking().FirstOrDefaultAsync(c => c.Id == id);
             if (sheet == null) return new NotFoundResult();
             return new JsonResult(sheet);
         }
